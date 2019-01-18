289ab63f155a20ebc74bb37ca09084d023dcb615
ilikegirls <26187718+halfbloodprince16@users.noreply.github.com>
2019-01-18 22:20:46 +0530

    Add files via upload

--- SLL.cpp
+++ SLL.cpp
@@ -8,123 +8,99 @@ priority_queue<int>pq;
 std::map<int,int> m;
 const int N=1e6+10;
 /*--------------------------hbp16@hbp16-Inspiron-3543:-$------------------------------*/
-struct node 
+
+struct node
 {
 	int data;
 	node *next;
 };
 struct node *head=NULL;
-ll size=0;
-void insert()
-{
-	int data;
-	cout<<"Enter data";
-	cin>>data;
-	node *temp = new node();
-	size++;
-	temp->data = data;
-	temp->next = head;
-	head = temp;
-}
 
 void display()
 {
-	cout<<"\n";
 	node *p = head;
 	while(p!=NULL)
 	{
-		cout<<p->data<<" ";
+		cout<<p->data<<"->";
 		p = p->next;
 	}
-	cout<<"\n";
+	cout<<endl;
 }
-void insert_in_bw()
+
+void insert()
 {
-	int data,pos;
-	cout<<"Enter Data:";
+	int data,pos,k=1;
+	cout<<"Enter Data :";
 	cin>>data;
-	cout<<"Enter Position:";
+	cout<<"Enter Pos :";
 	cin>>pos;
-	if(pos > size || pos < 0){cout<<"wrong posn bitch";return;}
-	int cnt=1;
-	node *p = head;
-	while(cnt <= pos-2)
-	{
-		cnt++;
-		p = p->next;
-	}
-	//cout<<cnt;
-	//cout<<p->data;
-	node *temp = new node();
-	node *t;
+
+	node *temp = new node;
 	temp->data = data;
-	size++;
-	t = p->next;
-    p->next = temp;
-    temp->next = t;
+
+	if(pos==1)
+	{
+		temp->next = head;
+		head = temp;
+	}
+	else
+	{
+		node *p =head;
+
+		while(p!=NULL && k<pos-1)
+		{
+			p = p->next;
+			k++;
+		}
+		temp->next = p->next;
+		p->next = temp;
+	}
+	
 }
 
 void del()
 {
-	int pos;
-	cout<<"Enter Position:";
+	int pos,k=1;
+	cout<<"Enter Pos :";
 	cin>>pos;
-	if(pos > size || pos < 0){cout<<"wrong posn bitch";return;}
-	int cnt=1;
-	node *p = head;
-	if(pos == 1)
+
+	if(pos==1)
 	{
-		head = p->next;
+		node *p = head;
+		head = head->next;
 		free(p);
-		size--;
-		return;
 	}
-	while(cnt < pos-1)
+	else
 	{
-		cnt++;
-		p = p->next;
+		node *p = head;
+		while(p != NULL && k<pos-1)
+		{
+			k++;
+			p = p->next;
+		}
+		node *t = p->next;
+		p->next = t->next;
+		free(t);
 	}
-	node *t;
-	t = p->next->next;
-	free(p->next);
-	p->next = t;
-	size--;
-	cnt=1;
-	return;
 }
 
-void reverse()
-{
-    node *current = head;
-    node *prev = NULL, *next = NULL;
-
-
-    while (current != NULL)
-    {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-    head = prev;
-}
 int main(int argc, char const *argv[])
 {
 	int ch;
+	
 	do
 	{
-		cout<<"1:Insert\n2:Display\n3:Insert in Between\n4:Delete\n5:Reverse";
+		cout<<"1:Insert\n2:Delete\n3:Display";
 		cin>>ch;
 		switch(ch)
 		{
 			case 1:insert();break;
-			case 2:display();break;
-			case 3:insert_in_bw();break;
-			case 4:del();break;
-			case 5:reverse();break;
+			case 2:del();break;
+			case 3:display();break;
 		}
 	}
-	while(ch!=6);
+	while(ch!=4);
 	
+
 	return 0;
 }
